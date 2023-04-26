3535a2150f2d034ca6a4f45a22f1d9776ad0269f
Nabin Paudyal <nabinpaudyal2057@gmail.com>
2023-04-26 09:51:44 +0545

    first commit

---
+++ hello_world.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  print("Hello world");
+  stdout.write("This is another way:");
+  print(5 * 3);
+  print(4 + 2);
+  var name = "hari";
+  print("my name is $name");
+}
