1a9ef25094fac0d5b54af6094b5c89d72a04c043
Nabin Paudyal <nabinpaudyal2057@gmail.com>
2023-04-28 17:31:07 +0545

    variable

---
+++ variable.dart
@@ -0,0 +1,24 @@
+void main() {
+  int abc = 23;
+
+  print(abc);
+
+  //  rules for creating the varible in dart
+
+  // 1) variable name only contain a-z,A-Z,_,$
+  var Abc = "hari";
+  int _abc = 34;
+  var $name = "haru";
+
+  // 2) varible name cannot start with a digit.
+  // varible names must start with letter , doller or an uderscore character.
+
+// int 1name = 23;
+  //3) varibale names are case- sensitive( age, Age, AGE are three differet varibale)
+  int roll = 20;
+  int Roll = 30;
+  int ROLL = 101;
+  print(roll);
+  print(Roll);
+  print(ROLL);
+}
